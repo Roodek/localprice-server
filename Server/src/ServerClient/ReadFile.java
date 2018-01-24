fd25a0a9dff78e78753204a20554de80d7af0a02
Roodek <radmatyja@gmail.com>
2018-01-24 22:34:54 +0100

    2 commit
    parsing a receipt and converting to Json

--- Server/src/ServerClient/ReadFile.java
+++ Server/src/ServerClient/ReadFile.java
@@ -3,6 +3,7 @@ package ServerClient;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadFile {
     public static void main(String[] args){
@@ -10,16 +11,84 @@ public class ReadFile {
         reader.readFile();
     }
 
-    public void readFile(){
+    public Record readFile(){
+        ArrayList<Record> products = new ArrayList<Record>();
+        Record finalRecord = new Record();
+
         String file = "C:\\Users\\user\\Desktop\\imgtest\\out.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
+
+            boolean read = false;
+            String shopName = br.readLine();
+            String shopAddres = br.readLine();
+
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                if(line.contains("PARAGON FISKALNY")){
+                    System.out.println("jest");
+                    read = true;
+                    continue;
+                }
+
+                if (line.toUpperCase().contains("SPRZED")){
+                    finalRecord = new Record(products);
+                    System.out.println("koniec czytania");
+                    break;
+                }
+                if(read == true){
+                    products.add(new Record(extractName(line),extractPrice(line)[0],extractPrice(line)[1],shopName,shopAddres));
+                    System.out.println(line);
+                    //System.out.println(extractName(line));
+                    //System.out.println(extractPrice(line));
+                    extractPrice(line);
+                }
+
+                //System.out.println(line);
             }
         }catch(IOException e){
             e.printStackTrace();
         }
+        /*
+        for (Record i : products){
+            i.show();
+        }*/
+        return finalRecord;
+    }
+    public String extractName(String line){
+        String result = new String();
+        for(int i = 0 ; i <line.length();i++){
+            if(line.charAt(i) == ','){
+                break;
+            }
+            result+=line.charAt(i);
+        }
+        result = result.substring(0,result.length()-3);
 
+        return result;
+    }
+    public double[] extractPrice(String line){//funkcja zwraca 2 ceny -detaliczna(extractPrice(line)[0]) oraz koncowa(extractPrice()line[0])
+
+        String productPrice = new String();
+        String finalPrice = new String();
+        int index=0;
+        int counter = 0;
+        for(int i = line.length()-1 ; i>-1;i--){
+            if(line.charAt(i) == ',' || line.charAt(i) =='.'){
+                counter+=1;
+                if(counter == 2){
+                    index = i+3;
+                    productPrice = line.substring(i+1,i+3);
+                    finalPrice = line.substring(index+1,line.length()-2);
+                }
+            }
+            if(line.charAt(i) == 'x' || line.charAt(i) =='*'){
+                productPrice = line.substring(i+1,index);
+                break;
+            }
+        }
+
+
+        return new double[]{Double.parseDouble(productPrice.replace(",", ".")),
+                Double.parseDouble(finalPrice.replace(",","."))};
     }
 }
--- Server/src/ServerClient/Record.java
+++ Server/src/ServerClient/Record.java
@@ -1,21 +1,38 @@
 package ServerClient;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Record {
-    double price;
-    String productName;
-    String shopName;
-    String addres;
+    private double price;
+    private double finalPrice;
+    private String productName;
+    private String shopName;
+    private String addres;
+    private double ammount = finalPrice/price;
+    private ArrayList<Record> productsList;
 
     public Record(){}
 
-    public Record(String name, double price, String shop, String addres){
+    public Record(String name, double price,double finalPrice, String shop, String addres){
 
         this.productName = name;
         this.price=price;
+        this.finalPrice = finalPrice;
         this.shopName=shop;
         this.addres=addres;
+    }
+
+    public  Record(ArrayList<Record> productsList) {
+        this.productsList = productsList;
     }
 
     public void setPrice(double price) {
@@ -45,8 +62,62 @@ public class Record {
         productName = jsonObject.getString("productName");
         price = jsonObject.getDouble("productPrice");
         addres = jsonObject.getString("shopLocation");
-        show();
+        //show();
+    }
+    public JSONObject RecordToJson(Record record){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.append("shopName",shopName);
+        jsonObject.append("productName",productName);
+        jsonObject.append("productPrice",price);
+        jsonObject.append("shopLocation",addres);
+
+        return jsonObject;
+    }
+    public JsonObject createJsonReceipt(){
+        JSONArray ja = new JSONArray();
+        JSONObject jo = new JSONObject();
+
+
+        for(Record i : productsList){
+            jo = new JSONObject();
+            ja.put(jo.append("name",i.productName).append("price",i.finalPrice).append("ammount",i.ammount));
+        }
+        String date = new String();
+        Map<String,String> config = new HashMap<String, String>();
+        JsonBuilderFactory factory = Json.createBuilderFactory(config);
+        JsonObject value = factory.createObjectBuilder()
+                .add("receipt",factory.createObjectBuilder()
+                    .add("date",date)
+                    .add("shop",factory.createObjectBuilder()
+                        .add("address",addres)
+                        .add("name",shopName))
+                    .add("products",factory.createArrayBuilder((JsonArray) ja)))
+                .build();
+
+        return value;
     }
 
 
 }
+/*
+"receipt":{
+        "date":"data z paragonu",
+        "shop":{
+            "address":"adres sklepu",
+            "name":"nazwa sklepu"
+            },
+        "produtcts":[
+            {
+            "name":"mleko",
+            "price":"1.99",
+            "ammount":"3"
+            },
+            {
+            "name":"ser",
+            "price":"6.47",
+            "ammount":"0.27"
+            }
+            ]
+        }
+        }
+        */
